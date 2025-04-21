07f895a82fdc38a24e04a5074f887e47f3bea21b
Gaag13 <giancarloaag@gmail.com>
2025-04-21 14:48:45 -0500

    update tools

--- CANTIDADES/Utils/Tools.cs
+++ CANTIDADES/Utils/Tools.cs
@@ -6,7 +6,6 @@ namespace CANTIDADES.Utils
     public static class Tools
     {
 
-#if REVIT2021_OR_GREATER
         public static double Feet_to_cm(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, UnitTypeId.Centimeters);
         public static double Feet_to_m(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, UnitTypeId.Meters);
         public static double Feet2_to_m2(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, UnitTypeId.SquareMeters);
@@ -16,17 +15,7 @@ namespace CANTIDADES.Utils
         public static double m_to_Feet(double length) => Autodesk.Revit.DB.UnitUtils.ConvertToInternalUnits(length, UnitTypeId.Meters);
         public static double mm_to_Feet(double length) => Autodesk.Revit.DB.UnitUtils.ConvertToInternalUnits(length, UnitTypeId.Millimeters);
 
-#else
-        public static double Feet_to_cm(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, DisplayUnitType.DUT_CENTIMETERS);
-        public static double Feet_to_m(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, DisplayUnitType.DUT_METERS);
-        public static double Feet2_to_m2(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, DisplayUnitType.DUT_SQUARE_METERS);
-        public static double Feet3_to_m3(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, DisplayUnitType.DUT_CUBIC_METERS);
 
-        public static double Feet_to_mm(double length) => Autodesk.Revit.DB.UnitUtils.ConvertFromInternalUnits(length, DisplayUnitType.DUT_MILLIMETERS);
-        public static double Cm_to_Feet(double length) => Autodesk.Revit.DB.UnitUtils.ConvertToInternalUnits(length, DisplayUnitType.DUT_CENTIMETERS);
-        public static double m_to_Feet(double length) => Autodesk.Revit.DB.UnitUtils.ConvertToInternalUnits(length, DisplayUnitType.DUT_METERS);
-        public static double mm_to_Feet(double length) => Autodesk.Revit.DB.UnitUtils.ConvertToInternalUnits(length, DisplayUnitType.DUT_MILLIMETERS);
-#endif
         
     }
 
